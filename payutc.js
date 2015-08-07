f0f90b122e6c393b82c433eb0dd1226917476e8d
Jo Colina <jo@jocolina.com>
2015-08-07 22:29:24 +0200

    Added License

--- payutc.js
+++ payutc.js
@@ -1,3 +1,27 @@
+// *****************************************************************************
+// The MIT License (MIT)
+
+// Copyright (c) 2015-2016 
+// Jo Colina - @jsmrcaga
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// *****************************************************************************
 function timeInSQL () {
 
 	var today = new Date();
@@ -35,8 +59,6 @@ function timeInSQL () {
 	// console.log(today);
 	return today;
 }
-
-// date.toISOString
 
 
 var payutcAPI = {
@@ -365,7 +387,7 @@ var payutc = {
 			//prix en euros
 			if (alcool == true) alcool = 0;
 			if (alcool == false) alcool =1;
-			return payutcAPI.genericApiCall("GESARTICLE", "setProduct", {name: name, parent: category, prix: price*100, stock: stock, alcool: alcool, 
+			return payutcAPI.genericApiCall("GESARTICLE", "setProduct", {name: name, parent: category, prix: price, stock: stock, alcool: alcool, 
 				image: image, fun_id: funId, obj_id: objId || null, tva: tva || "0.00", cotisant: cotisant || "1"});
 		},
 
